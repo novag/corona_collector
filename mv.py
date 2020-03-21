401feebabd3f314c438b432b0ec6d5478af09147
novag <git@hendrik.cc>
2020-03-21 01:14:18 +0100

    mv: Fix dt string

--- mv.py
+++ mv.py
@@ -90,7 +90,7 @@ class CoronaParser:
         table = self.tree.xpath('//table/tr')
 
         dt_text = table[1].xpath('td/p/strong/text()')[2]
-        dt = datetime.strptime(dt_text, 'Stand %d.%m. %H:%M').replace(year=2020).strftime('%Y-%m-%dT%H:%M:%SZ')
+        dt = datetime.strptime(dt_text, 'Stand %d.%m. %H:%M Uhr').replace(year=2020).strftime('%Y-%m-%dT%H:%M:%SZ')
 
         # Counties
         data = []
