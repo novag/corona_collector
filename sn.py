828ed76945edc76ddb6d9c1d5b6cdf9bd0042298
novag <git@hendrik.cc>
2020-03-27 23:07:01 +1100

    sn: Adapt changes

--- sn.py
+++ sn.py
@@ -102,8 +102,8 @@ class CoronaParser:
     def parse(self):
         dt_array = self.tree.xpath('//div[@class="text-col"]')[1].xpath('p/text()')
         for dt_text in dt_array:
-            if dt_text.startswith('Stand:'):
-                dt = datetime.strptime(dt_text, 'Stand: %d. %B %Y, %H:%M Uhr').strftime('%Y-%m-%dT%H:%M:%SZ')
+            if 'Stand:' in dt_text:
+                dt = datetime.strptime(dt_text.split('|\xa0')[-1], 'Stand: %d. %B %Y, %H:%M Uhr').strftime('%Y-%m-%dT%H:%M:%SZ')
                 break
 
         table = self.tree.xpath('//table')[0]
