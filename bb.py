de28cdac9428edadb332617aa8bdc0f8760bc7fe
novag <git@hendrik.cc>
2020-03-30 13:07:05 +1100

    bb: Adapt changes

--- bb.py
+++ bb.py
@@ -93,7 +93,7 @@ class CoronaParser:
     def parse(self):
         table = self.tree.xpath('//table/tbody/tr')
 
-        dt_text = table[0].xpath('td')[2].xpath('p/strong/text()')[1]
+        dt_text = table[0].xpath('td')[2].xpath('p/strong/text()')[-1]
         dt = datetime.strptime(dt_text, 'Stand: %d.%m., %H:%M Uhr').replace(year=2020).strftime('%Y-%m-%dT%H:%M:%SZ')
 
         # Counties
