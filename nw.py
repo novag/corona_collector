2e8e800c08f6864d1e341b96b1b7cab7df35654e
novag <git@hendrik.cc>
2020-04-20 17:52:44 +1000

    nw: Adapt changes

--- nw.py
+++ nw.py
@@ -104,8 +104,11 @@ class CoronaParser:
 
     def parse(self):
         dt_text = self.tree.xpath('//div[@class="group-introduction field-group-div"]//div[@class="field-item even"]/p/text()')[1]
-        dt_text = dt_text.split('Stand: ')[-1]
-        dt = datetime.strptime(dt_text, '%d. %B %Y, %H:%M Uhr. ').strftime('%Y-%m-%dT%H:%M:%SZ')
+        dt_text = dt_text.split('Stand: ')[-1].strip()
+        try:
+            dt = datetime.strptime(dt_text, '%d. %B %Y, %H:%M Uhr.').strftime('%Y-%m-%dT%H:%M:%SZ')
+        except:
+            dt = datetime.strptime(dt_text, '%d. %B %Y.').replace(hour=12).strftime('%Y-%m-%dT%H:%M:%SZ')
 
         counties_table = self.tree.xpath('//table')[0]
 
